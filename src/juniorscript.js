be0fe56534f882f04d0661175233fc53bc835106
JuniorCode <JuniorCode@users.noreply.github.com>
2018-05-01 21:53:58 -0400

    Update juniorscript.js

--- src/juniorscript.js
+++ src/juniorscript.js
@@ -140,12 +140,13 @@ for (var i = 0; i < tags.length; i++) {
   if (tags[i].type.toLowerCase().indexOf("juniorscript") > -1) {
      temp_code = tags[i].innerHTML;
      eval_jrs(temp_code);
+  }
 }
 
 var xmlHttp = new XMLHttpRequest();
 var url = "https://juniorscript-updates.herokuapp.com?version=alpha1";
 xmlHttp.open("GET", url, true);
 
-if (JSON.parse(xmlHttp.responseText)["update"] == true) {
+if (JSON.parse(xmlHttp.responseText).update == true) {
    console.log('%c You are using an outdated version of JuniorScript. Download the latest version at https://juniorscript.com', 'background: #2b2b2b; color: #ffffff');
 }
